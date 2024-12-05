a4695b0a2782624855faefab7a7df1dd4e506ce7
mitchell <mitchell@gici.com.au>
2024-12-05 14:41:18 +1100

    add legacy log collection for older variants of windows server

--- Patch Management/HealthCheck.cs
+++ Patch Management/HealthCheck.cs
@@ -13,7 +13,7 @@ namespace Patch_Management
 {
     public class HealthCheck
     {
-
+        static bool useLocalTrace = false;
 
         static readonly string[] errorCodes = {
             "80070032", //Feature update failure
@@ -31,6 +31,22 @@ namespace Patch_Management
 
         //C:\Windows\System32\tracerpt.exe
 
+        static bool CheckSymbolDepends()
+        {
+            //Some older versions of windows pull a copy of SymSrv.dll from Windows Defender.
+            string modulepath = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\Modules\\WindowsUpdate\\WindowsUpdateLog.psm1";
+            if (File.Exists(modulepath))
+            {
+                foreach (string line in File.ReadAllLines(modulepath))
+                {
+                    if (line.Contains("$SYMSRV_DLL_PATH = \"$env:ProgramFiles\\Windows Defender\\SymSrv.dll\""))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
 
         public static void GetWindowsUpdateLog(string outputFile)
         {
@@ -49,18 +65,54 @@ namespace Patch_Management
             //Remove any existing xml files.
             foreach (string file in Directory.GetFiles(tmpfolder, "*.xml"))
             {
-                System.IO.File.Delete(file);
+                File.Delete(file);
             }
 
             Console.WriteLine("Processing ETL Logs into C:\\Windows\\Temp\\PatchManagement");
+
+            useLocalTrace = CheckSymbolDepends();
+            if (useLocalTrace)
+            {
+                Console.WriteLine("Using local tracerpt mode.");
+                Directory.CreateDirectory(@"C:\Windows\Temp\PatchManagement\SymCache");
+                Directory.CreateDirectory(@"C:\Windows\Temp\PatchManagement\en-US");
+
+                if (!File.Exists(@"C:\Windows\Temp\PatchManagement\tracerpt.exe"))
+                {
+                    File.Copy(@"C:\Windows\System32\tracerpt.exe", @"C:\Windows\Temp\PatchManagement\tracerpt.exe");
+                }
+                if (!File.Exists(@"C:\Windows\Temp\PatchManagement\SymSrv.dll"))
+                {
+                    File.Copy(@"C:\Program Files\Windows Defender\SymSrv.dll", @"C:\Windows\Temp\PatchManagement\SymSrv.dll");
+                }
+                if (!File.Exists(@"C:\Windows\Temp\PatchManagement\en-US\tracerpt.exe.mui"))
+                {
+                    File.Copy(@"C:\Windows\System32\en-US\tracerpt.exe.mui", @"C:\Windows\Temp\PatchManagement\en-US\tracerpt.exe.mui");
+                }
+                if (!File.Exists(@"C:\Windows\Temp\PatchManagement\DbgHelp.dll"))
+                {
+                    File.Copy(@"C:\Windows\System32\DbgHelp.dll", @"C:\Windows\Temp\PatchManagement\DbgHelp.dll");
+                }
+
+
+            }
 
             //Convert each to XML.
             foreach (string etlfile in etlfiles)
             {
                 string filename = Path.GetFileName(etlfile);
                 Process conProc = new Process();
+
                 conProc.StartInfo.FileName = "C:\\Windows\\System32\\tracerpt.exe";
-                conProc.StartInfo.Arguments = etlfile + " -o " + Path.Combine(tmpfolder, filename) + ".xml -of XML";
+
+                if (useLocalTrace)
+                {
+                    conProc.StartInfo.WorkingDirectory = @"C:\Windows\Temp\PatchManagement\SymCache";
+                    conProc.StartInfo.FileName = @"C:\Windows\Temp\PatchManagement\tracerpt.exe";
+                }
+
+
+                conProc.StartInfo.Arguments = etlfile + " -o " + Path.Combine(tmpfolder, filename) + ".xml -of XML -pdb SRV*C:\\Windows\\Temp\\PatchManagement\\SymCache*http://msdl.microsoft.com/download/symbols -i C:\\Windows\\System32\\wuaueng.dll;C:\\Windows\\System32\\wuapi.dll;C:\\Windows\\System32\\wuuhext.dll;C:\\Windows\\System32\\wuuhmobile.dll;C:\\Windows\\System32\\wuautoappupdate.dll;C:\\Windows\\System32\\storewuauth.dll;C:\\Windows\\System32\\wuauclt.exe; -y";
                 conProc.StartInfo.UseShellExecute = false;
                 conProc.StartInfo.RedirectStandardOutput = true;
 
@@ -85,30 +137,56 @@ namespace Patch_Management
 
                 foreach (XmlNode eventNode in eventNodes)
                 {
+                    //Event/System/TimeCreated
                     XmlNode systemNode = eventNode.SelectSingleNode("ns:System", nsmgr);
                     XmlNode timeNode = systemNode.SelectSingleNode("ns:TimeCreated", nsmgr);
 
-
-
-                    XmlNode EventDataNode = eventNode.SelectSingleNode("ns:EventData", nsmgr);
-                    if (EventDataNode != null)
+                    
+                    if(!useLocalTrace)
                     {
-                        SW.Write(timeNode.Attributes["SystemTime"].Value);
-                        SW.Write(" ");
-
-                        XmlNodeList dataNodes = EventDataNode.SelectNodes("ns:Data", nsmgr);
-                        foreach (XmlNode dataNode in dataNodes)
+                        //Event/EventData
+                        XmlNode EventDataNode = eventNode.SelectSingleNode("ns:EventData", nsmgr);
+                        if (EventDataNode != null)
                         {
-                            if (dataNode.Attributes["Name"].Value.Equals("Info"))
-                            {
-                                SW.Write(dataNode.InnerText);
-                            }
-                        }
+                            SW.Write(timeNode.Attributes["SystemTime"].Value);
+                            SW.Write(" ");
 
-                        SW.WriteLine();
+                            XmlNodeList dataNodes = EventDataNode.SelectNodes("ns:Data", nsmgr);
+                            foreach (XmlNode dataNode in dataNodes)
+                            {
+                                if (dataNode.Attributes["Name"].Value.Equals("Info"))
+                                {
+                                    SW.Write(dataNode.InnerText);
+                                }
+                            }
+                            SW.WriteLine();
+
+                        }
+                    }
+                    else
+                    {
+                        //older version uses debug.
+                        //Event/DebugData
+                        XmlNode DebugDataNode = eventNode.SelectSingleNode("ns:DebugData", nsmgr);
+                        if (DebugDataNode != null)
+                        {
+                            SW.Write(timeNode.Attributes["SystemTime"].Value);
+                            SW.Write(" ");
+                            
+                            XmlNodeList MessageNode = DebugDataNode.SelectNodes("ns:Message", nsmgr);
+                            foreach(XmlNode MsgNode in MessageNode)
+                            {
+                                SW.Write(MsgNode.InnerText);
+                            }
+
+                            SW.WriteLine();
+                        }
+                        
+
                     }
 
 
+                    
                 }
 
             }
